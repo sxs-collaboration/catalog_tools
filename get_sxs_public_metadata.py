6f54f6824ed5cb5dc98e03302dc35770c594b9bd
Geoffrey Lovelace <geoffrey4444@gmail.com>
2019-07-11 11:21:05 -0700

    Get metadata from black-holes.org instead of arxiv

--- get_sxs_public_metadata.py
+++ get_sxs_public_metadata.py
@@ -12,11 +12,12 @@ from sxs import zenodo as zen
 import sys
 
 
-def resolutions_for_simulation(sxs_id, zenodo_metadata):
+def resolutions_for_simulation(sxs_id, sxs_catalog):
     """Returns a list of the available resolutions for a given sxs_id
-    and zenodo metadata"""
+    and sxs catalog metadata"""
     resolutions = []
-    files = zenodo_metadata[sxs_id]['files']
+    url = sxs_catalog['simulations'][sxs_id]['url']
+    files = sxs_catalog['records'][url]['files']
     for file in files:
         split_filename = file['filename'].split('/')
         if (str(split_filename[-1]) == "Horizons.h5"):
@@ -36,32 +37,30 @@ except:
     print("Error: output directory " + args.output_dir + " already exists.")
     exit(-1)
 
-# Get zenodo metadata: sxs_zenodo.json
-print("SXS zenodo metadata -> " + args.output_dir + "/sxs_zenodo.json")
-md = zen.api.Records.search(q='communities:sxs AND access_right:open')
-catalog_json = {}
-for simulation in md:
-    sxs_id = simulation['title'].split(' ')[-1]
-    if (len(sxs_id.split(':')) == 3):
-        if (sxs_id.split(':')[-2] == 'BBH'):
-            catalog_json[sxs_id] = simulation
-with open(args.output_dir + "/sxs_zenodo.json", 'w') as file:
-    file.write(json.dumps(catalog_json))
+# Get current black-holes.org catalog.json from black-holes.org
+print("Getting " + args.output_dir + "/sxs_catalog.json")
+request = requests.get("https://data.black-holes.org/catalog.json",
+                       headers={'accept': 'application/citeproc+json'})
+sxs_catalog = request.json()
+
+# Get list of SXS IDs for all BBH simulations
+bbh_simulations = []
+for simulation in sxs_catalog['simulations']:
+    if simulation.split(':')[-2] == "BBH":
+        bbh_simulations.append(simulation)
+
+# Save the metadata, adding a list of SXS IDs for BBH simulations
+sxs_catalog['bbh_simulation_keys'] = bbh_simulations
+with open(args.output_dir + "/sxs_catalog.json", 'w') as file:
+    file.write(json.dumps(sxs_catalog))
+
 
 # Generate sxs_zenodo_resolutions.json
 print("Generating " + args.output_dir + "/sxs_zenodo_resolutions.json")
 resolutions_available = {}
-for simulation in catalog_json:
+for simulation in bbh_simulations:
     resolutions_available[simulation] = \
-        resolutions_for_simulation(simulation, catalog_json)
+        resolutions_for_simulation(simulation, sxs_catalog)
 
 with open(args.output_dir + "/sxs_zenodo_resolutions.json", 'w') as file:
     file.write(json.dumps(resolutions_available))
-
-# Get arxiv.org catalog.json
-print("Getting " + args.output_dir + "/sxs_catalog_arxiv.json")
-request = requests.get("https://arxiv.org/src/1904.04831/anc/sxs_catalog.json",
-                       headers={'accept': 'application/citeproc+json'})
-sxs_catalog_arxiv = request.json()
-with open(args.output_dir + "/sxs_zenodo_arxiv.json", 'w') as file:
-    file.write(json.dumps(sxs_catalog_arxiv))
